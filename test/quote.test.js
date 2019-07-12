bb0a28a0fa2e612b68f15412cb839c4294cb68fa
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-12 16:50:37 -0700

    Renamed file

---
+++ test/quote.test.js
@@ -0,0 +1,18 @@
+const quote = require('../lib/middleware/quote');
+
+describe('quote middleware', () => {
+  it('responds with random quote using GET /random', done => {
+    const req = {};
+
+    const next = () => {
+      expect(req.quote).toEqual({
+        character: expect.any(String),
+        quote: expect.any(String),
+        image: expect.any(String)
+      });
+      done();
+    };
+
+    quote(req, {}, next);
+  });
+});
